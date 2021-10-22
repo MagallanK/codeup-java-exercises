712f86e3ee112bbe984710d7c7e0499a7db718b7
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-10-22 14:23:11 -0500

    Added lecture notes with ConsoleIO.java, and now adding ConsoleExercises.java for the exercises

---
+++ src/ConsoleIO.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ConsoleIO {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+//        String name = "Magallan";
+//        System.out.printf("Hello there, %s. Nice to see you. \n", name);
+//
+//
+//        String greeting = "Howdy";
+//        String cohortName= "Quasar";
+//        System.out.printf("%s, %s", greeting, cohortName);
+
+        System.out.print("Enter something: ");
+
+        String userInput = scanner.nextLine();
+
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+        //To display a % sign when using printf you double up the %% like so. Example
+        System.out.printf("Here is the example with a percent symbol included: --> %% %s", userInput);
+
+//        System.out.printf("You entered: %s", userInput);
+
+    }
+
+
+
+}
