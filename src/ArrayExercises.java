c0ece681c6abb3b80154ec9fb58ddc27ec3fbcbb
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-11-04 19:56:06 -0500

    Refactored ArraysExercises.java

--- src/ArrayExercises.java
+++ src/ArrayExercises.java
@@ -2,11 +2,16 @@ import java.util.Arrays;
 
 public class ArrayExercises {
 
+    //Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
+    public static Person[] addPerson(Person[] people, Person person1) {
+        Person[] newPeople = Arrays.copyOf(people, people.length + 1);
+        newPeople[newPeople.length - 1] = person1;
+        return newPeople;
+    }
 
-
-    public static void main(String[] args){
-//        What happens when you run the following code? Why is Arrays.toString necessary?
-            //The system prints out the memory code not a string showing each element of the array we want to access. Arrays.toString is necessary, so we can convert the original array to a string which allows us to see the array as a whole in a string layout which is accepted by the console.
+    public static void main(String[] args) {
+//What happens when you run the following code? Why is Arrays.toString necessary?
+// The system prints out the memory code not a string showing each element of the array we want to access. Arrays.toString is necessary, so we can convert the original array to a string which allows us to see the array as a whole in a string layout which is accepted by the console.
 
 //        int[] numbers = {1, 2, 3, 4, 5};
 //        System.out.println(numbers);
@@ -24,21 +29,12 @@ public class ArrayExercises {
         System.out.println("Person[1] = " + people[1].getName());
         System.out.println("Person[2] = " + people[2].getName());
 
-        for (Person person : people) {
+        Person person1 = new Person("Andrew");
+
+        Person[] people2 = addPerson(people, person1);
+
+        for (Person person : people2) {
             System.out.printf("%s is the name called%n", person.getName());
         }
-
-
-        Person[] addPerson = Arrays.copyOf(people, people.length + 1);
-        System.out.println("Person.length = " + people.length);
-        System.out.println("addPerson.length = " + addPerson.length);
-
-        addPerson[addPerson.length - 1] = new Person("Andrew");
-        for(Person peoplePlusOne : addPerson){
-            System.out.println(peoplePlusOne.getName());
-        }
-
-
     }
-//        Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
 }
